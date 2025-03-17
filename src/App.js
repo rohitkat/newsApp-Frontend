050195c60107881fea5357411eab66a763dfd3d2
Rohit Katoch <katoch.rohit@gmail.com>
2025-03-17 22:56:28 +0530

    Rendering Menus from API

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import HomePage from './pages/HomePage';
 import { Routes, Route } from 'react-router-dom';
 import TopLinks from './components/TopLinks.tsx';
 import Footer from './components/Footer.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <div className="App">
+      <QueryClientProvider client={queryClient}>
       <TopLinks />
       <NavBar />
       <Routes>
         <Route path='/' element={<HomePage />} />
       </Routes>
       <Footer />
+      </QueryClientProvider>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom'
 
 function NavBar() {
+
+  const fetchMenus = async() =>{
+    const {data} = await axios.get('http://localhost:5000/api/menus',{
+      withCredentials:true
+    });
+    console.log('Menus-',data);
+    return data;
+  }
+
+
+    const {data, isLoading, error} = useQuery({
+      queryKey:['menu'],
+      queryFn:fetchMenus,
+      staleTime:60000,
+    });
+  
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
